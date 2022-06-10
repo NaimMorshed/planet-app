62d0fc2da66c7e2322f205da39fdf2e659536173
Naim Morshed <morshed.naim13@gmail.com>
2022-06-10 19:29:59 +0600

    app initialized

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { colors } from './src/theme/colors';
+import { useFonts } from 'expo-font';
+import { typography } from './src/theme/typography';
+import { spacing } from './src/theme/spacing';
 
 export default function App() {
+  const [loaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/Spartan-Bold.ttf'),
+  })
+
+  if (!loaded) {
+    return (
+      <Text>Font is loading...</Text>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: typography.bold, fontSize: spacing[4] }}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +28,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.darkOrange,
     alignItems: 'center',
     justifyContent: 'center',
   },
